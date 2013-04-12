a3bce8a439597c4f6f57cb604a915cd1b6a06649
Kristen <kristenamorgan@mac.com>
2013-04-12 09:32:12 -0500

    implement call function added hotel and attraction data added radio buttons added toasts for areas that dont work

--- CodeSprint/HotelLocator/src/com/example/hotellocator/Favourites.java
+++ CodeSprint/HotelLocator/src/com/example/hotellocator/Favourites.java
@@ -3,6 +3,8 @@ package com.example.hotellocator;
 import java.util.ArrayList;
 
 import com.google.android.gms.maps.model.LatLng;
+
+import android.net.Uri;
 import android.os.Bundle;
 import android.content.Intent;
 import android.content.res.Configuration;
@@ -11,17 +13,27 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ExpandableListView;
+import android.widget.Toast;
 import android.widget.ExpandableListView.OnChildClickListener;
 
 public class Favourites extends Fragment {
 
 	final Hotel[] favouriteHotels = {
-			new Hotel("Pegasus", "7 Knutsford Boulevard", "Lorem Ipsum is simply dummy text of the printing and typesetting industry.", 1000, new LatLng(25.002856, -76.795659), 5),
-	    		new Hotel("Grande Palladium", "3 Old Way", "Boring", 1004, new LatLng(18.002856, 45.795659), 2),
-	};
+			new Hotel("Pegasus", "7 Knutsford Boulevard", "Lorem Ipsum is simply dummy text of the printing and typesetting industry.", "Kingston", "Kingston", new LatLng(25.002856, -76.795659)),
+			new Hotel("Akwaaba", "Silver Sands Estates, Duncans","Nice scenery", "Duncans", "Trelawney", new LatLng(-78.3671, 18.26805)),
+    		new Hotel("All Seasons Beach Resort", "Barrett  Hall, Greenwood, Lit", "Nice sandy beaches", "Greenwood", "St. James", new LatLng(-78.3666,18.26356)),
+    		new Hotel("Almond Tree", "Arawak P.O. Box 332", "Lovely weather", "Mammee Bay", "St. Ann", new LatLng(-78.3665,18.26398)),
+    		new Hotel("Hotel Four Seasons", "18 Ruthven Road", "Kalooki every Tuesday", "Kingston 10", "Kingston", new LatLng(-77.9041,18.3639)),
+    		new Hotel("Hotel Riu", "Tropical Bay Bloody Bay Negril", "Caters to Spanish speakers as well", "Negril", "Hanover", new LatLng(-77.8975,18.47607)),
+    		new Hotel("Blue Harbour","P.O. Box 50", "Best Jerk Spot within 5 minutes", "Port Maria","St. Mary",new LatLng(-78.354,18.24547)),
+    		new Hotel("Tia Maria Villa", "136 Silver Sands Estate, P.O.", "Dancing every other Wednesday", "Duncans", "Trelawney", new LatLng(-77.0723, 18.41486)),
+    		new Hotel("Honey Comb",	"Silver Sands Estate", "Complimentary Continental Breakfast", "Duncans", "Trelawney", new LatLng(-77.9114, 18.46007))
+    };
 	
 	final Attraction[] favouriteAttractions = {
-    		new Attraction("White River Valley", "boring", "Ocho Rios", "Daniel Melville", "Vaneka McKenzie", "Cascade", "Endevour", "St. Mary", new String[]{"974-2018","382-6907"}, new String[]{"River Tubing", "Horseback Riding", "Kayaking"} ),
+			new Attraction("Bob Marley Museum", "Historic", "Kingston", "Jacqueline Stewart", "Jacqueline Stewart", "56 Hope Road", "Kingston 6", "St. Andrew", new String[]{"927-9152"}, new String[]{"Tour of the former home of Bob Marley"}),
+		    new Attraction("KOOL RUNNINGS WATER PARK", "Delightful Negril vacation spot", "Negril",	"POINCIANA RESORTS Ltd", "MRS. NEKEISHA MYRIE-KOZER", "NORMAN MANLEY BLVD", null, null, new String[]{"957-5400", "957-5620"}, new String[]{"WATER SLIDES", "GO KARTS", "DINING", "CONFERENCE", "PAINT BALL", "RAFTING"}),
+			new Attraction("White River Valley", "boring", "Ocho Rios", "Daniel Melville", "Vaneka McKenzie", "Cascade", "Endevour", "St. Mary", new String[]{"974-2018","382-6907"}, new String[]{"River Tubing", "Horseback Riding", "Kayaking"} ),
     };
 	
 	ArrayList<String> favourites = new ArrayList<String>();
@@ -66,9 +78,10 @@ public class Favourites extends Fragment {
 					int groupPosition, int childPosition, long id) {
 				// TODO Auto-generated method stub
 				// call number
+				
 				String name = MyAdapter.parentList[groupPosition];
+				String type = determineType(name, favouriteHotels, favouriteAttractions);
 				if (childPosition==0) {
-					String type = determineType(name, favouriteHotels, favouriteAttractions);
 					Intent myIntent = new Intent();
 					myIntent.setClass(getActivity(), DetailsFragment.class);
 					if(type.equals("hotel")){
@@ -81,9 +94,35 @@ public class Favourites extends Fragment {
 						}
 				    startActivity(myIntent);
 				}
-				else{if(childPosition==1){
-					//call hotel/attraction
-				}}
+				else{
+					if(childPosition==1 && type.equals("attraction") ){
+					int position = -1;
+					Toast.makeText(getActivity(),
+							name,
+								Toast.LENGTH_SHORT).show();
+					for(int i=0;i<favouriteAttractions.length;i++)
+					{
+						if(favouriteAttractions[i].getName().equals(name))
+						{
+							position = i;
+							break;
+						}
+					}	
+				
+					if(position>-1){
+						Intent callIntent = new Intent(Intent.ACTION_CALL);
+						callIntent.setData(Uri.parse("tel:"+ favouriteAttractions[position].getContact()[0])); 
+						startActivity(callIntent);
+					}
+				}
+				else{
+					if(childPosition==1 && type.equals("hotel")){
+							Toast.makeText(getActivity(),
+									"No contact number",
+										Toast.LENGTH_SHORT).show();
+							}
+						}
+					}
 				return false;
 			}
 			
--- CodeSprint/HotelLocator/src/com/example/hotellocator/SearchFragment.java
+++ CodeSprint/HotelLocator/src/com/example/hotellocator/SearchFragment.java
@@ -17,27 +17,42 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageButton;
 import android.widget.ListView;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
 import android.widget.Toast;
 
 public class SearchFragment extends ListFragment {
 	final Hotel[] hotels = {
-			new Hotel("Pegasus", "7 Knutsford Boulevard", "Lorem Ipsum is simply dummy text of the printing and typesetting industry.", 1000, new LatLng(25.002856, -76.795659), 5),
-    		new Hotel("Hilton", "7 Hibiscus Drive", "Lorem ipsum", 1001, new LatLng(18.002856, -76.795659), 4),
-    		new Hotel("Four Seasons", "2A Shalimar Close", "Eureka", 1002, new LatLng(28.002856, -76.795659), 6),
-    		new Hotel("Holiday Inn", "83 Lore Lane", "Exciting", 1003, new LatLng(18.002856, 76.795659), 3),
-    		new Hotel("Grande Palladium", "3 Old Way", "Boring", 1004, new LatLng(18.002856, 45.795659), 2),
-    		new Hotel("Riu", "Ocho Rios", "nice food", 1005, new LatLng(14.002856, -7.795659), 4)
+			new Hotel("Pegasus", "7 Knutsford Boulevard", "Lorem Ipsum is simply dummy text of the printing and typesetting industry.", "Kingston", "Kingston", new LatLng(25.002856, -76.795659)),
+			new Hotel("Akwaaba", "Silver Sands Estates, Duncans","Nice scenery", "Duncans", "Trelawney", new LatLng(-78.3671, 18.26805)),
+    		new Hotel("Alamanda Inn", "39 Salem Beach, P.O. Box 65", "Lovely Beaches", "Runaway Bay", "St. Ann", new LatLng(-78.3669,18.27206)),
+    		new Hotel("Alfred Ocean Place",	"Norman Manley Blvd. P.O. Box", "Luscious Palm Trees", "Negril", "Westmoreland", new LatLng(-78.3669, 18.27165)),
+    		new Hotel("Alhambra Inn", "1 Tucker Avenue", "Close to city", "Kingston 3", "Kingston", new LatLng(-78.3666,18.26627)),
+    		new Hotel("All Seasons Beach Resort", "Barrett  Hall, Greenwood, Lit", "Nice sandy beaches", "Greenwood", "St. James", new LatLng(-78.3666,18.26356)),
+    		new Hotel("Almond Tree", "Arawak P.O. Box 332", "Lovely weather", "Mammee Bay", "St. Ann", new LatLng(-78.3665,18.26398)),
+    		new Hotel("Altamont Court Hotel", "1-3 Altamont Terrace", "5 Swimming pools", "Kingston 5", "Kinston", new LatLng(-78.3665,18.27204)),
+    		new Hotel("Hotel Four Seasons", "18 Ruthven Road", "Kalooki every Tuesday", "Kingston 10", "Kingston", new LatLng(-77.9041,18.3639)),
+    		new Hotel("Hotel Riu", "Tropical Bay Bloody Bay Negril", "Caters to Spanish speakers as well", "Negril", "Hanover", new LatLng(-77.8975,18.47607)),
+    		new Hotel("Blue Harbour","P.O. Box 50", "Best Jerk Spot within 5 minutes", "Port Maria","St. Mary",new LatLng(-78.354,18.24547)),
+    		new Hotel("Seaview Villa","Seaview District, Southfield", "Single and Double Occupany available", "Southfield",	"St. Elizabeth", new LatLng(-77.163,18.4268)),
+    		new Hotel("Tia Maria Villa", "136 Silver Sands Estate, P.O.", "Dancing every other Wednesday", "Duncans", "Trelawney", new LatLng(-77.0723, 18.41486)),
+    		new Hotel("Honey Comb",	"Silver Sands Estate", "Complimentary Continental Breakfast", "Duncans", "Trelawney", new LatLng(-77.9114, 18.46007))
     };
 	
 	final Attraction[] attractions = {
     		new Attraction("Xayamaca", "fun","Montego Bay", "Mr. Paul Hastings", "Dalton Hastings", "Howard Cooke Blvd, Freeport", "Montego Bay", "St. James", new String[]{"844-9935"}, new String[]{"Plant Centre", "Complex Grounds", "Gift Shop", "Juice Bar"}),
     		new Attraction("White River Valley", "boring", "Ocho Rios", "Daniel Melville", "Vaneka McKenzie", "Cascade", "Endevour", "St. Mary", new String[]{"974-2018","382-6907"}, new String[]{"River Tubing", "Horseback Riding", "Kayaking"} ),
     		new Attraction("Veronica Park", "nice food", "Montego Bay", "Mr. Brasco Lee", "Michael Lee Chin", "Allsides District", "Waita Bit P.O.", "Trelawney", new String[]{"538-8940", "468-9449"}, null),
-    		new Attraction("Two Sister's Cave", "scary", "Kingston", "UCD", "Mrs. Winsome Roache", "Hellshire", null, "St. Catherine", null, new String[]{"Arawak Cave"} )
+    		new Attraction("Two Sister's Cave", "scary", "Kingston", "UCD", "Mrs. Winsome Roache", "Hellshire", null, "St. Catherine", null, new String[]{"Arawak Cave"}),
+    		new Attraction("Bob Marley Museum", "Historic", "Kingston", "Jacqueline Stewart", "Jacqueline Stewart", "56 Hope Road", "Kingston 6", "St. Andrew", new String[]{"927-9152"}, new String[]{"Tour of the former home of Bob Marley"}),
+    		new Attraction("Caymanas Track Ltd.", "Caymanas Park is Jamaica's only race track.", "Kingston", "Mr. Michael Sibbles"," Mr. Michael Sibbles", "Caymanas Park Complex", "Gregory Park P.O.", "St. Catherine", new String[]{"988-2523", "988-2524", "988-2525", "988-2526"}, new String[]{"Horse Racing", "Children Entertainment", "Dining"}),
+    		new Attraction("KOOL RUNNINGS WATER PARK", "Delightful Negril vacation spot", "Negril",	"POINCIANA RESORTS Ltd", "MRS. NEKEISHA MYRIE-KOZER", "NORMAN MANLEY BLVD", null, null, new String[]{"957-5400", "957-5620"}, new String[]{"WATER SLIDES", "GO KARTS", "DINING", "CONFERENCE", "PAINT BALL", "RAFTING"})
     };
 	
 	ArrayAdapter<String> listAdapter;
 	ImageButton searchButton;
+	RadioButton rButton;
+	RadioGroup filters;
 	Button aroundMe;
 	EditText query;
 	ArrayList<String> displayList = new ArrayList<String>();
@@ -61,18 +76,28 @@ public class SearchFragment extends ListFragment {
 				}
 	      		
 	      	});
+	      	
+	      	filters = (RadioGroup)rootView.findViewById(R.id.filters);
+	      	int selectedId = filters.getCheckedRadioButtonId();
+	      	rButton = (RadioButton) rootView.findViewById(selectedId);
+	      	
 	      	searchButton = (ImageButton)rootView.findViewById(R.id.searchButton);
 	      	searchButton.setOnClickListener(new OnClickListener(){
 
 				@Override
 				public void onClick(View arg0) {
 					// TODO Auto-generated method stub
-					System.out.println(query.getText().toString());
+					Toast.makeText(getActivity(),
+							rButton.getText(), Toast.LENGTH_SHORT).show();
 					searchHotels(query.getText().toString());
 					
 				}
 	      		
 	      	});
+	      	
+	      
+	      	
+	      		      	
 	      	for (int i=0;i<hotels.length;i++){
 				displayList.add(hotels[i].getHotelName());
 			}
@@ -89,15 +114,32 @@ public class SearchFragment extends ListFragment {
 	protected void searchHotels(String query) {
 		// TODO Auto-generated method stub
 		ArrayList<String> found = new ArrayList<String>();
-		
-		for (int i=0;i<hotels.length;i++){	
-			if(hotels[i].getHotelName().contains(query)){
-				found.add(hotels[i].getHotelName());
+      	
+
+		if(rButton.getText().equals("By Name")){
+			for (int i=0;i<hotels.length;i++){	
+				if(hotels[i].getHotelName().contains(query)){
+					found.add(hotels[i].getHotelName());
+				}
+			}
+			for (int i=0;i<attractions.length;i++){	
+				if(attractions[i].getName().contains(query)){
+					found.add(attractions[i].getName());
+				}
 			}
 		}
-		for (int i=0;i<attractions.length;i++){	
-			if(attractions[i].getName().contains(query)){
-				found.add(attractions[i].getName());
+		else{
+			if(rButton.getText().equals("By Region")){
+				for (int i=0;i<hotels.length;i++){	
+					if(hotels[i].getHotelName().contains(query)){
+						found.add(hotels[i].getHotelName());
+					}
+				}
+				for (int i=0;i<attractions.length;i++){	
+					if(attractions[i].getRegion().contains(query)){
+						found.add(attractions[i].getRegion());
+					}
+				}
 			}
 		}
 		
